a13935209bd8374bb44d04818363391068a50868
weidehai <243395655@qq.com>
2021-03-17 23:24:14 +0800

    test

--- upload.cmd
+++ upload.cmd
@@ -1,4 +1,4 @@
-echo "cd /home/weidehai" > sftp_cmd.txt
-echo "mkdir jenkins-test && cd jenkins-test" >> sftp_cmd.txt
-echo "put README.md ./" >> sftp_cmd.txt
-sftp weidehai < sftp_cmd.txt
+echo cd /home/weidehai > sftp_cmd.txt
+echo mkdir jenkins-test && cd jenkins-test >> sftp_cmd.txt
+echo put README.md ./ >> sftp_cmd.txt
+sftp -F C:\Users\24339\.ssh weidehai < sftp_cmd.txt
