da9ed5c8ab9c09d32752bd74b3b220c09c1e2371
weidehai <243395655@qq.com>
2021-03-17 23:34:47 +0800

    test

--- upload.cmd
+++ upload.cmd
@@ -1,4 +1,5 @@
 echo cd /home/weidehai > sftp_cmd.txt
-echo mkdir jenkins-test && cd jenkins-test >> sftp_cmd.txt
+echo mkdir jenkins-test >> sftp_cmd.txt
+echo cd jenkins-test >> sftp_cmd.txt
 echo put README.md ./ >> sftp_cmd.txt
 sftp -F ./config weidehai < sftp_cmd.txt
